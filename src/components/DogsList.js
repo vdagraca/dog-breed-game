b1bb07a43dc99b5e443d07d7845f36346dab0b60
Vivian da Graca <vivian.da.graca@gmail.com>
2019-04-08 15:46:01 +0200

    Doglist react

---
+++ src/components/DogsList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+// import { Link } from ‘react-router-dom’
+
+
+export default class DogsList extends Component {
+    //   renderDogBreed(breed) {
+    //     return      <li key={breed}>
+    //     <Link to={ `/dog-breeds/${breed}` }>{breed}</Link>
+    //   </li>
+
+    //   }
+
+    render() {
+        const { dogBreeds } = this.props
+        return (
+            <div className="dogs-list">
+
+                <h1>Dogs List</h1>
+
+                {!dogBreeds && 'Loading...'}
+
+                {
+                    dogBreeds &&
+                    <ul>{this.props.dogBreeds.map(breed => <li key={breed}>{breed}</li>)}</ul>
+                }
+            </div>
+        )
+    }
+}
