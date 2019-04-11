9ce11d3785618d85ff9ab8dbd689cc6f91a61959
angbacic <angela.bacic1@gmail.com>
2019-04-11 14:21:09 +0200

    css

--- src/navigation/Home.js
+++ src/navigation/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import './Home.css'
+import home from './puppies.jpg'
 
 
 
 const Home = () => {
     return (
-        <div>
-            <p> Home page</p>
+        <div className="body">
+            <h1> Welcome Doglovers! </h1>
+            <p>Enter the world of puppies and puppettes,
+                learn about breeds and test your knowledge in our
+                 Dog Quizzies!</p>
+            <img src={home}/>
         </div>
 
     )
